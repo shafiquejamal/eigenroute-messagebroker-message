0b72a03edec54c32ad1586344b5883592f73099d
Shafique Jamal <shafique.jamal@gmail.com>
2017-01-25 13:43:22 -0500

    Add toJson method

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := """eigenroute-messagebroker-message"""
 
-version := "0.0.1"
+version := "0.0.2"
 organization := "com.eigenroute"
 
 scalaVersion := "2.11.7"
--- src/main/scala/com/eigenroute/plumbing/MessageBrokerMessage.scala
+++ src/main/scala/com/eigenroute/plumbing/MessageBrokerMessage.scala
@@ -1,7 +1,11 @@
 package com.eigenroute.plumbing
 
+import play.api.libs.json.JsValue
+
 trait MessageBrokerMessage {
 
   def messageType: MessageBrokerMessageType
+
+  def toJson: JsValue
 
 }
